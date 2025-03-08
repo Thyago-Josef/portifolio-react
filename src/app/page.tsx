10592b7218711ba930e0e25b56a73cf9553df305
Thyago-Josef <t.y_josef7charm@hotmail.com>
2025-03-08 19:48:53 -0300

    atualizando parte do cabeçário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,22 +16,29 @@ export default function Home() {
 
 
         <div className="quadro-cima">
-
+          <div className="elementos-cima">
+            <div className="home">Home</div>
+            <div className="sobre mim">Sobre mim</div>
+            <div className="projetos">Projetos</div>
+            <div className="contato">Contato</div>
+          </div>
 
           <div className="nome-centro">Olá,visitante!<br />
             <p className="nome">Me chamo Thyago</p>
-            sou um desenvolvedor Full Stack</div>
-
-          <div className="elementos-cima">
-            <div className="contato">Contato</div>
-            <div className="sobre">Sobre</div>
-            <div className="home">Home</div>
+            Sou um desenvolvedor Full Stack, apaixonado por tecnologia
           </div>
 
         </div>
 
+        <div className="sobre-mim">
+          <h2>Sobre mim</h2>
+          <p>Me chamo Thyago, tenho 34 anos e sou desenvolvedor Full Stack.</p> <p> Sou formado em Análise e Desenvolvimento de SIstemas e estou Cursando Engenharia da Computação.</p>
+          <p>Tenho vasto conhecimento em Desenvolvimento web e desktop, utilizando Java com Springboot e Python com Jango</p>
+          <p>E no Front end tenho conhecimento com Angular, React e HTML, CSS e JavaScript.</p>
+        </div>
 
       </main>
+
     </div>
   );
 }
